9de1f745f276130a30a55e315a8320e1671054a0
momo1002 <momoko.nakada1002@gmail.com>
2017-08-17 10:46:21 -0700

    add Assignment1

---
+++ Assignment1/Assignment1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment1
+//
+//  Created by Momoko Nakada on 2017-08-17.
+//  Copyright © 2017 Momoko Nakada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
